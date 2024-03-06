3dfdcfe1c74940ac08056a0c9568736ecfe48748
Diane Chiang <tchiang0@uw.edu>
2024-03-05 16:22:06 -0800

    Finished test cases, revised landing and prediction page

---
+++ tests/test_streamlit.py
@@ -0,0 +1,19 @@
+"""test_streamlit.py"""
+import unittest
+import streamlit as st
+from streamlit.testing.v1 import AppTest
+
+
+class TestStreamlit(unittest.TestCase):
+    def test_landing_page(self):
+        """ Test landing page displays brain info markdowns """
+        at = AppTest.from_file("ui_demo/Brain_Tumor_Information.py").run()
+        # st.title and st.markdown
+        assert len(at.markdown) == 8
+
+
+    def test_prediction_page(self):
+        """ Test brain prediction page displays relevant info """
+        at = AppTest.from_file("ui_demo/pages/Brain_Tumor_Prediction.py").run()
+        # st.title and st.markdown
+        assert len(at.markdown) == 2
